06cdb559487030d5176dbda566a9bbe631672145
hsrokon <hsrokonofficial@gmail.com>
2025-07-20 02:13:27 +0600

    mongodb handled to new ownner, uri updated

--- api/index.js
+++ api/index.js
@@ -8,7 +8,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aczhr3x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gvjpk31.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aczhr3x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gvjpk31.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
